79b2ddcf5463d6471eef98d3d3dc691c82bc3c79
JaimeCs27 <jaimecab2002@gmail.com>
2024-05-08 22:44:04 -0600

    a

--- src/components/HeaderProfesorGuiaCoord.jsx
+++ src/components/HeaderProfesorGuiaCoord.jsx
@@ -15,13 +15,13 @@ const Header = () => {
         <div className="flex items-center">
           <img src={logo} alt="TEC Logo" className="h-6 mr-4" />
           <nav className="flex">
-            <Link to={'ProfesorGuiaCoordinador/EquipoDeTrabajo'}>
+            <Link to={'/ProfesorGuiaCoordinador/EquipoDeTrabajo'}>
               Equipo de trabajo
             </Link>
-            <Link to={'ProfesorGuiaCoordinador/Estudiantes'}>
+            <Link to={'/ProfesorGuiaCoordinador/Estudiantes'}>
               Estudiantes
             </Link>
-            <Link to={'ProfesorGuiaCoordinador/PlanDeTrabajo'}>
+            <Link to={'/ProfesorGuiaCoordinador/PlanDeTrabajo'}>
               PlanDeTrabajo
             </Link>
           </nav>
